170aac2c7ad6d12aff5843b46fb93011c3e1df6e
AhmedNegm5 <a.negm1231@gmail.com>
2025-02-15 18:52:15 +0200

    edit syntax

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,31 +3,30 @@ import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import customError from "../utils/customError.js";
 
+const signup = asyncHandler(async (req, res, next) => {
+  const { firstName, lastName, email, password, role } = req.body;
 
-const signup = asyncHandler(async (req, res,next) => {
-    const { firstName, lastName, email, password, role } = req.body;
+  const existUser = await User.findOne({ email });
 
-    const existUser = await User.findOne({ email });
+  if (!email || !password) {
+    return next(new customError("Please provide an email and password", 400));
+  }
 
-    if(!email || !password){
-        return next(new customError("Please provide an email and password", 400));
-    }
+  if (existUser) {
+    return next(new customError("User already exist", 400));
+  }
 
-    if(existUser){
-        return next(new customError("User already exist", 400));
-    }
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    password,
+    role,
+  });
 
-    const user = new User({
-        firstName,
-        lastName,
-        email,
-        password,
-        role
-    })
-    await user.save();
+  await user.save();
 
-    res.status(201).json({message: "User created successfully"});
-    
+  res.status(201).json({ message: "User created successfully" });
 });
 
-export {signup};
+export { signup };
