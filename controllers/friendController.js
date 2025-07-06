bddd362d88e90c3fc96cfcea4059ea9e7b18760f
AhmedNegm5 <a.negm1231@gmail.com>
2025-07-06 19:09:48 +0300

    fix get all friends

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -284,12 +284,12 @@ const getAllFriends = asyncHandler(async (req, res, next) => {
 
   // Prepare response with populated data
   const friends = validFriends.map((person) => ({
-    firstName: person.user.firstName,
-    lastName: person.user.lastName,
+    customFirstName: person.user.firstName,
+    customLastName: person.user.lastName,
     user: {
       _id: person.user._id,
-      customFirstName: person.customFirstName,
-      customLastName: person.customLastName,
+      firstName: person.customFirstName,
+      lastName: person.customLastName,
       email: person.user.email,
     },
   }));
